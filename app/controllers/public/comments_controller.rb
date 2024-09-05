865ac926224c8490f10a3a7d795d78db90c840a9
EC2 Default User <ec2-user@ip-172-31-45-163.ap-northeast-1.compute.internal>
2024-09-05 16:44:36 +0000

    投稿一覧と詳細作成

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,9 +1,16 @@
-class Public::CommentController < ApplicationController
+class Public::CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
     @comment.user = current_user
+    
     if @comment.save
+      if params[:rating][:score].present?
+        @rating = @post.ratings.build(score: params[:rating][:score])
+        @rating.user = current_user
+        @rating.save
+      end
+
       redirect_to @post, notice: 'コメントが追加されました。'
     else
       redirect_to @post, alert: 'コメントの追加に失敗しました。'
