035eaad586be208eb794674be5feef0eb0348ed0
EC2 Default User <ec2-user@ip-172-31-45-163.ap-northeast-1.compute.internal>
2024-09-16 04:53:50 +0000

    タグ管理機能作成

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -1,10 +1,29 @@
 class Admin::TagsController < ApplicationController
+  before_action :authenticate_admin!
   def index
+    @tags = Tag.all
+    @tag = Tag.new
   end
 
   def create
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      redirect_to admin_tags_path, notice: 'タグが作成されました'
+    else
+      @tag = Tag.all
+      render :index, alert: 'タグの作成に失敗しました'
+    end
   end
 
   def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+    redirect_to admin_tags_path, notice: 'タグが削除されました'
+  end
+  
+  private
+  
+  def tag_params
+    params.require(:tag).permit(:name)
   end
 end
