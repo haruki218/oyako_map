ea7365de854ad988d1ba3411e2f5177c1cdcb04c
EC2 Default User <ec2-user@ip-172-31-45-163.ap-northeast-1.compute.internal>
2024-09-20 20:24:47 +0000

    タグページを更新

--- app/controllers/public/tags_controller.rb
+++ app/controllers/public/tags_controller.rb
@@ -2,7 +2,26 @@ class Public::TagsController < ApplicationController
   
   def show
     @tag = Tag.find(params[:id])
-    @posts = @tag.posts.order(created_at: :desc)
+    @facility_type = params[:facility_type]
+    @posts = @tag.posts
+    # 施設タイプによる絞り込み
+    if @facility_type.present?
+      @posts = @posts.where(facility_type: @facility_type)
+    end
+    # ソートの適用
+    if params[:latest]
+      @posts = @posts.latest
+    elsif params[:old]
+      @posts = @posts.old
+    elsif params[:highly_rated]
+      @posts = @posts.highly_rated
+    elsif params[:most_commented]
+      @posts = @posts.most_commented
+    else
+      @posts = @posts.latest
+    end
+    # ページネーション
+    @posts = @posts.page(params[:page]).per(8)
   end
   
   def create
